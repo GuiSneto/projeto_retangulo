ea87d144e37ac2ff26f0edc2b1b4dc53e3552df2
Guilherme Staciarini <staciarinineto@gmail.com>
2020-05-20 21:00:10 -0300

    Programa finalizado

--- src/projeto_retangulo/retangulo.java
+++ src/projeto_retangulo/retangulo.java
@@ -12,6 +12,7 @@ public class retangulo {
 		Scanner sc = new Scanner(System.in); // leitura de dados
 		
 		double LARG,ALT;
+		int MEN,i=0;
 		
 		//Leitura da largura
 		System.out.print("Digite a largura do retangulo: ");
@@ -31,13 +32,36 @@ public class retangulo {
 		while (ALT < 0) {
 			System.out.print("O valor deve ser positivo. Tente novamente: ");
 			ALT = sc.nextDouble();
+			System.out.println("");
 		}
-		
-		sc.close();
-		
-		
 
-		
+		//Validação menu	
+		while (i == 0) {	
+			System.out.println("");
+			System.out.println("MENU:");
+			System.out.println("1 - Mostrar area");
+			System.out.println("2 - Mostrar perimetro");
+			System.out.println("3 - Sair");
+			System.out.print("Digite um valor: ");
+			MEN = sc.nextInt();
+			System.out.println("");
+				
+			if (MEN == 1) {
+				System.out.printf("AREA = %.1f%n",(ALT*LARG));
+
+			}
+				
+				else if (MEN == 2) {
+					System.out.printf("PERIMETRO = %.1f%n",(2*ALT + 2*LARG));
+
+				}
+				else if (MEN == 3) {
+					i = +1;
+					System.out.println("FIM DO PROGRAMA!");
+				}
+			
+		}			
+		sc.close();		
 		
 	}
 
